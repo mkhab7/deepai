6b073c36ef73dbb5c5f1900b7bc7328977f6fce6
solid <myappgo123@gmail.com>
2022-03-11 18:09:40 +0330

    Added output method

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -2,42 +2,33 @@
 
 namespace Solid\Deepai\Response;
 
-use ErrorException;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 
 class Response
 {
-    protected string $url;
+    protected ?string $url = null;
+
+    protected ?string $output = null;
 
     /**
      * @param object $data
      */
-    public function __construct( protected object $data)
+    public function __construct(protected object $data)
     {
-        $this->url = $this->data->output_url;
+        $this->url = $this?->data?->output_url ?? null;
+
+        $this->output = $this->url ?? $this->data?->output ?? null;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getUrl(): string
+    public function url(): ?string
     {
         return $this->url;
     }
 
-    /**
-     * @throws GuzzleException
-     */
-    public function save(string $path): bool
-    {
-        $client = new Client();
-        $client->get(
-            $this->url,
-            ['sink' => $path]
-        );
-        return true;
-    }
 
     /**
      * get all the response data
@@ -48,4 +39,33 @@ class Response
     {
         return $this->data;
     }
+
+    /**
+     * returns url/text output
+     *
+     *
+     * @return string|null
+     */
+    public function output(): ?string
+    {
+        return $this->output;
+    }
+    /**
+     * @throws GuzzleException
+     * @throws \Exception
+     */
+    public function save(string $path): bool
+    {
+        if (!filter_var($this->url, FILTER_VALIDATE_URL))
+            throw new \Exception('Error : Invalid url !');
+
+        $client = new Client();
+        $client->get(
+            $this->url,
+            ['sink' => $path]
+        );
+        return true;
+    }
+
+
 }
